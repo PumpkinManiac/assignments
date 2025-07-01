8277de38c5a03af11b18e46ca29ba1259c00d8f3
Peeyush <115183346+PumpkinManiac@users.noreply.github.com>
2025-07-01 22:53:53 +0530

    Update anagram.js

--- week-2/week-2-js/easy/anagram.js
+++ week-2/week-2-js/easy/anagram.js
@@ -5,7 +5,12 @@
 */
 
 function isAnagram(str1, str2) {
+// base case length 
+  if (str1.length !== str2.length) return false;
+  const str1Sorted = str1.toLowerCase().split("").sort().join("");
+  const str2Sorted = str2.toLowerCase().split("").sort().join("");
 
+  return str1Sorted == str2Sorted; 
 }
 
 module.exports = isAnagram;
